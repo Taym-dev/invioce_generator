1d8ecdcece7e78ae867b91fba89f60781d3570c7
Tibbe R. <143705358+DVCTibbeR@users.noreply.github.com>
2024-04-18 13:01:06 +0200

    json invoice

--- data.py
+++ data.py
@@ -9,18 +9,19 @@ json_in_path = f"{this_path}/JSON_IN"
 json_procecsed = f"{this_path}/JSON_PROCESSED"
  
 onlyfiles = [f for f in listdir(json_in_path) if isfile(join(json_in_path, f))]
- 
+
 for fileName in onlyfiles:    
 	file = open(f"{json_in_path}/{fileName}")
 	file_data = json.load(file)["order"]
 
+	# PDF
 	INVOICE_INFO = {
 		"id": file_data["ordernummer"],
 		"createdAt": file_data["orderdatum"],
 		"expiresAt": "24-04-2024",
 		"term": int(match("\d+", file_data["betaaltermijn"]).group())
 	}
- 
+
 	CLIENT_INFO = {
 		"name": file_data["klant"]["naam"],
 		"address": file_data["klant"]["adres"],
@@ -29,5 +30,31 @@ for fileName in onlyfiles:
 	}
 
 	createInvoice(INVOICE_INFO, CLIENT_INFO, file_data["producten"], f"INVOICE/{file_data['ordernummer']}.pdf")
- 
+
 	replace(f"{json_in_path}/{fileName}", f"{json_procecsed}/{fileName}")
+
+	# JSON
+	invoice = {
+		"products": [],
+		"totalWithoutTax": 0,
+		"tax": 0,
+		"total": 0
+	}
+
+	for product in file_data["producten"]:
+		invoice["products"].append({
+			"amount": product["aantal"],
+			"name": product["productnaam"],
+			"priceOne": product["prijs_per_stuk_excl_btw"],
+			"priceAll": product["prijs_per_stuk_excl_btw"] * product["aantal"],
+			"taxPercentage": product["btw_per_stuk"]
+		})
+
+		invoice["totalWithoutTax"] += product["prijs_per_stuk_excl_btw"]
+		invoice["tax"] += product["prijs_per_stuk_excl_btw"] * (product["btw_per_stuk"] / 100)
+
+	invoice["total"] = invoice["totalWithoutTax"] + invoice["tax"]
+
+	with open(f"{this_path}/INVOICE/{fileName}") as file:
+		json.dump(invoice, file)
+
